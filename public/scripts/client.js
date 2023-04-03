3c66d9b5301335b60a43c30c630471b36c17dc81
Yonin408 <justin12689@gmail.com>
2023-04-03 14:24:45 -0700

    submitted tweets are displayed on the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,39 +9,45 @@
 
 $(document).ready(function() { // use jQuery document ready function to ensure that all elements in the DOM are loaded before manipulating them
     const $form = $(`#create-tweet`);
-   
-   
-   $form.on('submit', (event) => {
-      event.preventDefault();
-      const urlencoded = $form.serialize();
-      console.log(urlencoded)
-      $.ajax({
-          method: "POST",
-          url: '/tweets',
-          data: urlencoded
-      }).then((response) => {
-          console.log(response);
-      });
-   });
-   
-   
+
+    $form.on('submit', (event) => {
+        event.preventDefault();
+        const urlencoded = $form.serialize();
+        console.log(urlencoded)
+        $('#tweet-text').val('');
+        $.ajax({
+            method: 'POST',
+            url: '/tweets',
+            data: urlencoded
+        }).then((response) => {
+            console.log(response)
+            loadTweets();
+            $('#tweets-container').empty();
+        })
+  
+    });
    const renderTweets = function(tweets) {
       for (const key of tweets) {
          const $tweet = createTweetElement(key)
-         $('#tweets-container').append($tweet);
+         $('#tweets-container').prepend($tweet);
       }
     }
    
    
    const createTweetElement = function(obj) {
-   const $tweet = (`     <article class ="oBorder">
+   const $tweet = (`  
+   <article class ="oBorder">
    <header class="pBorder">
-    <label for="uName" class="uName">${obj.user.name}</label>
+   <div class="main-content">
+   <img src="${obj.user.avatars}">
+    <label for="uName" class="uName">${obj.user.name}</label>\
+    <div class="space"></div>
     <label for="uAccount"class="handle">${obj.user.handle}</label>
+    </div>
     ${obj.content.text}
    </header>
    <span class="date"></span>
-   <script>$('.date').text(timeago.format(${obj.created_at}));</script>
+   <script>$('.date').text(timeago.format((${obj.created_at})));</script>
    <button class="interact"><i class="fa-solid fa-flag"></i></button>
    <button class="interact"><i class="fa-solid fa-retweet"></i></button>
    <button class="interact"><i class="fa-sharp fa-solid fa-heart"></i></button>
@@ -60,7 +66,7 @@ $(document).ready(function() { // use jQuery document ready function to ensure t
         console.log('Error: ', error);
       });
   }
-  
+
    loadTweets();
     });
    
--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
+const { tweets } = require("./lib/in-memory-db");
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
@@ -33,8 +33,22 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
-app.post('/tweets', (req, res) => {
 
+app.get('/tweets', (req, res) => {
+  res.json(tweets);
+});
+
+app.post('/tweets', (req, res) => {
+const text = req.body.text;
+
+const newTweet = {
+  text,
+  created_at: new Date().getTime(),
+};
+
+tweets.push(newTweet);
+console.log(tweets)
+res.status(201).send();
 })
 
 app.listen(PORT, () => {
