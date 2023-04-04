45cad437f772e9b807eb6f2207e08f934ed884a3
Yonin408 <justin12689@gmail.com>
2023-04-04 03:07:42 -0700

    Error messages

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
+    $('.error-empty').hide();
+    $('.error-full').hide();
     $("#tweet-text").on('click', function() {
         let max = 140;
         let count = document.getElementById("counter");
         let counter = document.getElementById('tweet-text')
         $("#tweet-text").on('input', () => {
             count.textContent = max - counter.value.length;
-            if (count.textContent === 140) {
-                alert('error, empty tweet');
+             if(count.textContent < 0) {
+                $('.error-full').slideDown().show();
             }
-            else if(count.textContent < 0) {
-                alert('character count exceeded limit');
-                return;
+            else {
+                $('.error-full').slideUp().hide();
+                event.preventDefault();
             }
-            
         });
+        $("#create-tweet").on('submit', () => {
+            count.textContent = max - counter.value.length;
+            if (count.textContent === 140) {
+                $('.error-empty').slideDown().show();
+
+                }
+        })
     });
   });
  
